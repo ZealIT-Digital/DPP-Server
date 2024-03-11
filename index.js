f036ca29ef96458017c585a0c0eb235df4360003
ZealIT-Digital <zealitdigital@gmail.com>
2024-03-11 18:09:03 +0530

    added postProductDetailsUI Endpoint

--- index.js
+++ index.js
@@ -22,13 +22,8 @@ import {
 dotenv.config();
 const app = express();
 
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -229,9 +224,10 @@ app.get("/getUiTemplate/:id", async (req, res) => {
   res.send(UiTemplate);
 });
 
-app.post("/postProductDetailsUI", async (eq, res) => {
+app.post("/postProductDetailsUI", async (req, res) => {
   let data = req.body;
   const postedTemplate = await postUiTemplate(data);
+  console.log(postedTemplate);
   res.send(postedTemplate);
 });
 
