32ca0615e22d62d931b843a91344e8716435fdec
Lokesh-Kanna-V <vlokeshkanna@gmail.com>
2024-05-13 19:23:47 +0530

    LK_1305_1923

--- routes/uidata.js
+++ routes/uidata.js
@@ -1,50 +1,60 @@
 import express from "express";
-let router = express.Router()
-import{
+let router = express.Router();
+import {
   getUiTemplate,
   getUiMasterTemplatebyCategory,
   getAllUiId,
   postUiTemplate,
   updateUi,
   templateID,
-  updateTempRunningNo
- }
- from "../helpers/UiHelper.js"
+  updateTempRunningNo,
+  deleteMasterTemplate,
+} from "../helpers/UiHelper.js";
 router.get("/productUiTemplate/:id", verifyToken, async (req, res) => {
-    jwt.verify(req.token, "DPP-Shh", async (err, authData) => {
-      if (err) {
-        res.sendStatus(403);
-      } else {
-        let { id } = req.params;
-        let prodDetails = await getProductsById(id);
-  
-        let tempId = prodDetails.templateId;
-  
-        let templateDetails = await getUiTemplate(tempId);
-        res.send(templateDetails);
-      }
-    });
-  });
-  // Middleware function to verify JWT
-function verifyToken(req, res, next) {
-    const bearerHeader = req.headers["authorization"];
-    if (typeof bearerHeader !== "undefined") {
-      const bearer = bearerHeader.split(" ");
-      const bearerToken = bearer[1];
-      req.token = bearerToken;
-      next();
-    } else {
+  jwt.verify(req.token, "DPP-Shh", async (err, authData) => {
+    if (err) {
       res.sendStatus(403);
+    } else {
+      let { id } = req.params;
+      let prodDetails = await getProductsById(id);
+
+      let tempId = prodDetails.templateId;
+
+      let templateDetails = await getUiTemplate(tempId);
+      res.send(templateDetails);
     }
+  });
+});
+// Middleware function to verify JWT
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
   }
-  router.get("/getUiTemplate/:id", async (req, res) => {
-    let { id } = req.params;
-    const UiTemplate = await getUiTemplate(id);
-    res.send(UiTemplate);
-  });
-  
+}
+router.get("/getUiTemplate/:id", async (req, res) => {
+  let { id } = req.params;
+  const UiTemplate = await getUiTemplate(id);
+  res.send(UiTemplate);
+});
+
 router.get("/getAllUiId", async (req, res) => {
-    let uiIds = await getAllUiId();
-    res.send(uiIds);
-  });
-export const uiRouter = router
+  let uiIds = await getAllUiId();
+  res.send(uiIds);
+});
+
+router.delete("/deleteMasterTemplate/:id", async (req, res) => {
+  let { id } = req.params;
+  console.log(id);
+
+  // let deleted = deleteMasterTemplate(id);
+
+  res.send(id);
+});
+
+export const uiRouter = router;
