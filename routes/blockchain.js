fd789fd597f871bf7ccc6009733a0fda41593dbe
saraevanabp66 <saraevanabp66@gmail.com>
2024-05-29 19:34:55 +0530

    sk_1706_290524

--- routes/blockchain.js
+++ routes/blockchain.js
@@ -5,13 +5,30 @@ import jwt from "jsonwebtoken";
 import { addData } from "../blockChain/blockchain.js";
 import { retrieveData } from "../blockChain/newretrive.js";
 
+// router.post(`/post`, async (req, res) => {
+//   let data = req.body;
+
+//   let bcResult = await addData(data);
+//   let transactionHash = bcResult.transactionHash;
+//   console.log(transactionHash);
+//   res.send(bcResult);
+// });
 router.post(`/post`, async (req, res) => {
   let data = req.body;
 
-  let bcResult = await addData(data);
-  let transactionHash = bcResult.transactionHash;
-  console.log(transactionHash);
-  res.send(bcResult);
+  try {
+    let bcResult = await addData(data);
+    let transactionHash = bcResult.transactionHash;
+    console.log(transactionHash);
+
+    // Assuming you have obtained the status from your blockchain
+    let status = "success"; // You should replace this with the actual status
+
+    res.send({ transactionHash, status });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Internal server error" });
+  }
 });
 
 router.get(`/retrieve/:id`, async (req, res) => {
