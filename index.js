29555b15d442b3610cb95c5d849f6ab91769b853
Lokesh-Kanna-V <vlokeshkanna@gmail.com>
2024-06-03 16:07:15 +0530

    LK_0306_1607

--- index.js
+++ index.js
@@ -23,7 +23,11 @@ import { blockchainRouter } from "./routes/blockchain.js";
 dotenv.config();
 const app = express();
 
-const allowedOrigins = ["https://devdpp.vercel.app", "http://localhost:3000"];
+const allowedOrigins = [
+  "https://devdpp.vercel.app",
+  "http://localhost:3000",
+  "https://dpp-client-dev.vercel.app",
+];
 
 app.use(
   cors({
