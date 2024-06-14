19405d29a9b789ddf016457ef6ae7480be3de621
Lokesh-Kanna-V <vlokeshkanna@gmail.com>
2024-06-14 15:07:32 +0530

    LK_1406_1507

--- routes/connection.js
+++ routes/connection.js
@@ -1,7 +1,13 @@
 let router = express.Router();
 import express, { response } from "express";
 import jwt from "jsonwebtoken";
-import { client } from "../index.js";
+import {
+  getallConnection,
+  postConnectionHeader,
+  postConnectionParams,
+  connectionID,
+  updateConnectionRunningNo,
+} from "../helpers/connectionHelper.js";
 
 // Middleware function to verify JWT
 function verifyToken(req, res, next) {
@@ -15,6 +21,7 @@ function verifyToken(req, res, next) {
     res.sendStatus(403);
   }
 }
+
 router.get("/getAllConnection", verifyToken, async (req, res) => {
   jwt.verify(req.token, "DPP-Shh", async (err, authData) => {
     if (err) {
@@ -26,12 +33,50 @@ router.get("/getAllConnection", verifyToken, async (req, res) => {
     }
   });
 });
-const getallConnection = async () => {
-  const getAllConnection = await client
-    .db("DigitalProductPassport")
-    .collection("ConnectionMasterData")
-    .find()
-    .toArray();
-  return getAllConnection;
-};
+
+router.post("/postConnectionHeader", verifyToken, async (req, res) => {
+  jwt.verify(req.token, "DPP-Shh", async (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      let data = req.body;
+
+      let idDetails = await connectionID();
+      let prefix = idDetails.prefix;
+      let running = idDetails.runningNumber;
+      let rangeStart = idDetails.rangeStart;
+      let rangeEnd = idDetails.rangeEnd;
+
+      let inc = parseInt(running) + 1;
+      let id = prefix + "-" + inc;
+
+      if (inc > rangeStart && inc < rangeEnd) {
+        updateConnectionRunningNo(inc);
+
+        data.id = id;
+
+        const postedConnection = await postConnectionHeader(data);
+
+        res.send(postedConnection);
+      } else {
+        res.send({ message: "ID Range did not match" });
+      }
+    }
+  });
+});
+
+router.put("/postConnectionParams/:id", verifyToken, async (req, res) => {
+  jwt.verify(req.token, "DPP-Shh", async (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      let { id } = req.params;
+      let data = req.body;
+      const connectionParam = await postConnectionParams(id, data);
+      res.send(connectionParam);
+      return connectionParam;
+    }
+  });
+});
+
 export const connectionRouter = router;
