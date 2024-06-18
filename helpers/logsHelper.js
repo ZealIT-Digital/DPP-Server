f2158618f5b7657bfa3067333414a0ce71e50563
saraevanabp66 <saraevanabp66@gmail.com>
2024-06-18 17:49:38 +0530

    sk_1749_1806

--- helpers/logsHelper.js
+++ helpers/logsHelper.js
@@ -56,8 +56,17 @@ async function getAllLogs(
 async function GetLogs(startDate, endDate, type, action) {
   try {
     // Format dates to ensure they are strings in 'YYYY-MM-DD' format
-    const start = startDate;
-    const end = endDate;
+    const start = formatDate1(startDate);
+    const end = formatDate1(endDate);
+    function formatDate1(dateString) {
+      console.log("date", dateString);
+      const dateObject = new Date(dateString);
+      console.log("dateobject", dateObject);
+      const day = dateObject.getDate().toString().padStart(2, "0");
+      const month = (dateObject.getMonth() + 1).toString().padStart(2, "0");
+      const year = dateObject.getFullYear();
+      return `${day}/${month}/${year}`;
+    }
 
     // Construct the query object
     const query = {
